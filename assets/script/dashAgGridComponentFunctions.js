9fe1f07673903bb4f94944a1e12c6a7f909f3663
Tanguy9862 <tanguy98.surowiec@gmail.com>
2024-05-01 05:06:58 +0200

    Refactor and enhance home.py with improved data handling and UI components
    - Introduce type hinting and documentation for better code clarity and maintenance.
    - Implement advanced data filtering mechanisms for dynamic year-based drug approval displays.
    - Add new KPI visualizations and interactive components like tooltips and modal dialogs.
    - Upgrade layout aesthetics with CSS styling adjustments for a cleaner and more modern UI.
    - Optimize callback functions for performance and readability.

---
+++ assets/script/dashAgGridComponentFunctions.js
@@ -0,0 +1,37 @@
+var dagcomponentfuncs = (window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {});
+
+dagcomponentfuncs.DMC_ActionIcon = function (props) {
+    const {setData, data, rowIndex} = props;  // Use data to access row data
+
+    function onClick() {
+        setData({
+            rowIndex: rowIndex,
+            drugName: data.drug_name,
+            dateApproval: data['Date of Approval']
+        });
+    }
+
+    const iconElement = React.createElement(window.dash_iconify.DashIconify, {
+        icon: props.icon,
+        color: props.iconColor,
+        width: props.iconWidth,
+        height: props.iconHeight
+    });
+
+    return React.createElement(
+        window.dash_mantine_components.ActionIcon,
+        {
+            onClick: onClick,
+            variant: props.variant,
+            style: {
+                margin: props.margin,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: props.marginTop,
+                marginLeft: props.marginLeft,
+            }
+        },
+        iconElement
+    );
+};
--- layouts/home_layout.py
+++ layouts/home_layout.py
@@ -1,7 +1,8 @@
 from operator import itemgetter
-from typing import Dict, Any
+from typing import List, Dict, Any
 
 import dash_mantine_components as dmc
+from dash.development.base_component import Component
 from dash_iconify import DashIconify
 
 
@@ -11,12 +12,26 @@ BORDER_RADIUS = 10
 
 
 def make_container(
-        children,
-        style_height,
-        pt=PADDING_TOP,
-        pl=PADDING_LEFT,
-        **kwargs
-):
+        children: List[Component],
+        style_height: str,
+        pt: int = PADDING_TOP,
+        pl: int = PADDING_LEFT,
+        **kwargs: Dict[str, Any]
+) -> dmc.Container:
+    """
+    Creates a stylized container for Dash components.
+
+    Args:
+        children (List[Component]): A list of Dash components to include within the container.
+        style_height (str): The height CSS property value for the container.
+        pt (int): Padding top value.
+        pl (int): Padding left value.
+        **kwargs (Dict[str, Any]): Additional keyword arguments to include extra styles.
+
+    Returns:
+        dmc.Container: A styled container component that encapsulates the children components.
+    """
+
     extra_styles = kwargs.get('extra_styles', {})
     return dmc.Container(
         children=children,
@@ -33,49 +48,51 @@ def make_container(
     )
 
 
-def assemble_kpi_panel(kpi_item):
+def assemble_kpi_panel(kpi_item: Dict[str, str]) -> dmc.Group:
+    """
+    Constructs a panel for displaying a key performance indicator (KPI) using badges and text.
+
+    Args:
+        kpi_item (Dict[str, str]): Dictionary containing label, icon, and color for the KPI.
+
+    Returns:
+        dmc.Group: A component group that visually represents the KPI with an icon and labels.
+    """
 
     label, icon, color = kpi_item['label'], kpi_item['icon'], kpi_item['color']
 
-    return dmc.Col(
+    return dmc.Group(
         [
-            dmc.Group(
+            DashIconify(
+                icon=icon,
+                width=40,
+                height=45,
+                color=color
+            ),
+            dmc.Stack(
                 [
-                    DashIconify(
-                        icon=icon,
-                        width=40,
-                        height=45,
-                        color=color
+                    dmc.Text(
+                        label.replace('-', ' '),
+                        transform='uppercase',
+                        weight=200,
+                        style={'font-size': '0.9375'}
                     ),
-                    dmc.Stack(
-                        [
-                            dmc.Text(
-                                label.replace('-', ' '),
-                                transform='uppercase',
-                                weight=200,
-                                style={'font-size': '0.9375'}
-                                # size=15
-                            ),
-                            dmc.Text(
-                                None,
-                                transform='capitalize',
-                                weight=500,
-                                # size=16 if label != 'last-updated' else 14,
-                                style={'font-size': '1rem'},
-                                id=f'{label}-kpi'
-                            )
-                        ],
-                        style={'height': '100%'},
-                        justify='center',
-                        ml='xs',
-                        spacing=0
+                    dmc.Text(
+                        None,
+                        transform='capitalize',
+                        weight=500,
+                        style={'font-size': '1rem'},
+                        id=f'{label}-kpi'
                     )
                 ],
-                mt=5,
-                spacing='sm'
+                style={'height': '100%'},
+                justify='center',
+                ml='xs',
+                spacing=0
             )
         ],
-        span=3,
+        mt=5,
+        spacing='sm'
     )
 
 
@@ -106,8 +123,8 @@ def make_tooltip_layout(hover_data: Dict[str, Any]) -> dmc.Group:
                 mr=8,
                 style={
                     'background-color': color,
-                    'width': '12px',
-                    'height': '12px',
+                    'width': '10px',
+                    'height': '10px',
                     'border-radius': '15px',
                     'border': 'none'
                 }
@@ -126,3 +143,101 @@ def make_tooltip_layout(hover_data: Dict[str, Any]) -> dmc.Group:
         }
     )
 
+
+def make_modal() -> dmc.Modal:
+    """
+    Creates a modal component for displaying detailed information about a drug.
+
+    Returns:
+        dmc.Modal: A Dash Mantine Component modal that includes various child components like badges and texts,
+                   configured for displaying drug-related information.
+    """
+
+    return dmc.Modal(
+            id='modal-detailed-drug',
+            title=None,
+            children=[
+                dmc.Group(
+                    [
+                        dmc.Tooltip(
+                            label='Target disease category for this medication. Indicates the primary area of '
+                                  'treatment.',
+                            children=[
+                                dmc.Badge(None, id='badge-disease-type', variant='gradient', size='md',
+                                          gradient={"from": "teal", "to": "cyan", "deg": 45})
+                            ],
+                            transition='fade',
+                            withArrow=True,
+                            position='bottom'
+                        ),
+                        dmc.Tooltip(
+                            label='Pharmacological class of the medication. Refers to the type of action the drug has '
+                                  'on the body.',
+                            children=[
+                                dmc.Badge(None, id='badge-drug-type', variant='gradient', size='md', color='#4C9F70',
+                                          gradient={"from": "grape", "to": "pink", "deg": 35})
+                            ],
+                            transition='fade',
+                            withArrow=True,
+                            position='bottom'
+                        ),
+                        dmc.Tooltip(
+                            label='Method of drug administration. Describes how the drug is delivered to the patient.',
+                            children=[
+                                dmc.Badge(None, id='badge-mode-administration', variant='gradient', size='md',
+                                          color='#4C9F70',
+                                          gradient={"from": "orange", "to": "red"}),
+                            ],
+                            transition='fade',
+                            withArrow=True,
+                            position='bottom'
+                        )
+                    ],
+                    spacing='xs',
+                    position='left',
+                    mt=-10
+                ),
+                dmc.Text(
+                    None,
+                    id='drug-description',
+                    mt=35
+                ),
+                dmc.Group(
+                    [
+                        dmc.Container(
+                            [
+                                dmc.Text('treatment for', transform='uppercase', size=16),
+                                dmc.Text(None, id='modal-treatment-for', weight=500, transform='uppercase'),
+                            ],
+                            style={'maxWidth': '70%'},
+                            m=0,
+                            px=0
+                        ),
+                        dmc.Container(
+                            [
+                                dmc.Text('developed by', transform='uppercase', size=16),
+                                dmc.Text(None, id='modal-company', weight=500, transform='uppercase'),
+                            ],
+                            m=0,
+                            px=0
+                        )
+                    ],
+                    position='left',
+                    mt='xl',
+                    mb='md',
+                    style={'align-items': 'flex-start'},
+                    spacing=50
+                )
+            ],
+            size='40%',
+            transition='pop',
+            styles={
+                'title': {'font-size': '30px', 'font-weight': '400', 'margin-top': '5px'},
+                'body': {'margin-top': '25px'},
+                'modal': {
+                    'background': 'linear-gradient(to bottom, #82c7a5 0%, #ffffff 100%)',
+                    'border-radius': '15px'
+                },
+                'close': {'margin-top': '-50px', 'color': 'grey'}
+            }
+        )
